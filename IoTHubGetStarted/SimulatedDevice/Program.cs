34cadedc31037eca81e7a3085871d02028370989
Isabel Cabezas Martín <isacabe@microsoft.com>
2018-04-16 17:35:45 +0200

    SimulatedDevice

---
+++ IoTHubGetStarted/SimulatedDevice/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Azure.Devices.Client;
+using Newtonsoft.Json;
+
+namespace SimulatedDevice
+{
+    class Program
+    {
+
+        static DeviceClient deviceClient;
+        static string iotHubUri = "tempIoTHub1234.azure-devices.net";
+        static string deviceKey = "tdvspvxYHVK3FJjSRueF9gHmeMxaoWIlSMMGOkazqYo=";
+
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Simulated device\n");
+            deviceClient = DeviceClient.Create(iotHubUri, new DeviceAuthenticationWithRegistrySymmetricKey("myFirstDevice", deviceKey), TransportType.Mqtt);
+            deviceClient.ProductInfo = "HappyPath_Simulated-CSharp";
+            SendDeviceToCloudMessagesAsync();
+
+
+            ReceiveC2DAsync();
+
+            Console.ReadLine();
+
+
+        }
+
+        private static async void SendDeviceToCloudMessagesAsync()
+        {
+            double minTemperature = 20;
+            double minHumidity = 60;
+            int messageId = 1;
+            Random rand = new Random();
+
+            while (true)
+            {
+                double currentTemperature = minTemperature + rand.NextDouble() * 15;
+                double currentHumidity = minHumidity + rand.NextDouble() * 20;
+
+                var telemetryDataPoint = new
+                {
+                    messageId = messageId++,
+                    deviceId = "myFirstDevice",
+                    temperature = currentTemperature,
+                    humidity = currentHumidity
+                };
+                var messageString = JsonConvert.SerializeObject(telemetryDataPoint);
+                var message = new Message(Encoding.ASCII.GetBytes(messageString));
+
+                message.Properties.Add("temperatureAlert", (currentTemperature > 30) ? "true" : "false");
+
+                await deviceClient.SendEventAsync(message);
+
+                Console.WriteLine("{0} > Sending message: {1}", DateTime.Now, messageString);
+
+                await Task.Delay(1000);
+            }
+        }
+
+        private static async void ReceiveC2DAsync()
+        {
+            Console.WriteLine("\nReceiving cloud to device messages from service");
+            while (true)
+            {
+                Message receivedMessage = await deviceClient.ReceiveAsync();
+                if (receivedMessage == null) continue;
+
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Received message: {0}", Encoding.ASCII.GetString(receivedMessage.GetBytes()));
+                Console.ResetColor();
+
+                await deviceClient.CompleteAsync(receivedMessage);
+            }
+        }
+    }
+}
